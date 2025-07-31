bfef02e2b7caaecbf429e1c68cdc342a7eccc2e1
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-07-31 23:01:47 +0100

    feat: admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path='/admin' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='ListBlog' element={<ListBlog />} />
+          <Route path='Comments' element={<Comments />} />
+        </Route>
       </Routes>
+
     </div>
   )
 }
---
+++ client/src/pages/admin/Comments.jsx
@@ -0,0 +1,7 @@
+const Comments = () => {
+  return (
+    <div>Comment</div>
+  )
+}
+
+export default Comments
