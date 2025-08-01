2a099517942bb02f0fcd8ce56e8102e8cd6f4f2b
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-08-01 01:44:56 +0100

    feat: admin login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,18 @@ import Dashboard from './pages/admin/Dashboard'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
+import Login from './components/admin/Login'
 
 const App = () => {
   return (
     <div>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path='/admin' element={<Layout />}>
+
+        {/* Admin Routes */}
+        <Route path='/admin' element={true ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path='addBlog' element={<AddBlog />} />
           <Route path='ListBlog' element={<ListBlog />} />
