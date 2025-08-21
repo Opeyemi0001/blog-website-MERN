5094828658d55e4f27757e9109255a7c2b874033
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-08-21 21:36:19 +0100

    feat: admin login

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,6 +3,7 @@ import { adminLogin } from '../controllers/adminController.js';
 
 const adminRouter = express.Router();
 
+// Route for admin login
 adminRouter.post("/login", adminLogin);
 
 export default adminRouter;
