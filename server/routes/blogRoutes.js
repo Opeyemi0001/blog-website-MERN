d970b428541d818c695e9357fc4cdb99879339b1
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-08-23 03:44:49 +0100

    feat: routes for blog

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from "../controllers/blogController.js";
 import upload from "../configs/middleware/multer.js";
 import auth from "../configs/middleware/auth.js";
 
@@ -7,5 +7,9 @@ const blogRouter = express.Router();
 
 // Route to add a new blog 
 blogRouter.post("/add", upload.single('image'), auth, addBlog);
+blogRouter.get('/all', getAllBlogs);
+blogRouter.get('/:blogId', getBlogById);
+blogRouter.post('/delete',auth,  deleteBlogById);
+blogRouter.post('/toggle-publish',auth,  togglePublish);
 
 export default blogRouter;
