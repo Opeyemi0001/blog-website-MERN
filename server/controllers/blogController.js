44ae0cbfa6c9040a89bacdd3faf2cc74ef70fabd
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-08-23 03:38:30 +0100

    feat: blog list, details, delete, and update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -4,6 +4,7 @@ import Blog from '../models/Blog.js';
 
 
 
+// Add a new blog
 export const addBlog = async (req, res) => {
   try {
     const {title, subTitle, description, category, isPublished} = JSON.parse(req.body.blog);
@@ -51,4 +52,49 @@ export const addBlog = async (req, res) => {
   }
 }
 
+// Get all published blogs
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({isPublished: true})
+    res.json({success: true, blogs})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
 
+// Get blog by ID
+export const getBlogById = async (req, res) => {
+  try {
+    const {blogId} = req.params;
+    const blog = await Blog.findById(blogId);
+    if(!blog) {
+      return res.json({success:false, message: "Blog not found"})
+    } 
+    res.json({success: true, blog})
+  } catch (error) {
+    res.json({success: false, message: error.message })
+  }
+}
+
+// Delete blog by ID
+export const deleteBlogById = async (req, res) => {
+  try {
+    const {id} =req.body;
+    await Blog.findByIdAndDelete(id);
+    res.json({success: true, message: "Blog deleted successfully"})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({success: true, message: "Blog status updated"})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
